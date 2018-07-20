54339bef5072bb9ab19ccf43f13fa4b23884e0eb
YannickLeRoux <rasyann@hotmail.com>
2018-07-19 20:18:22 -0400

    Preload Images

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -4,8 +4,8 @@ import React from 'react';
 const SocialIcons = () => {
         return (
           <div className="social-icons">
-              <a className="social-icon" href="https://www.instagram.com/FindTheHare/"><i className="fa fa-facebook-square social-media-icon"></i></a>
-              <a className="social-icon" href="https://www.facebook.com/FindTheHare"><i className="fa fa-instagram social-media-icon"></i></a>
+              <a className="social-icon" href="https://www.facebook.com/FindTheHare/"><i className="fa fa-facebook-square social-media-icon"></i></a>
+              <a className="social-icon" href="https://www.instagram.com/FindTheHare"><i className="fa fa-instagram social-media-icon"></i></a>
           </div>
         );
     };
